6fce18f5117e542aebadbd2f60fa756642664f03
Marcelo Dieguez <mdieguezdev@gmail.com>
2022-06-22 13:53:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,8 +6,13 @@ let homeContainer = document.querySelector('.home-container')
 let modalTimeEl = document.querySelector('#modal-time-el')
 let startAgainButton = document.querySelector('.start-again-el')
 
-let startingMinutes = 2;
+let startingMinutes = 10;
 let time = startingMinutes * 1;
+
+function start() {
+    startingMinutes = 20;
+    time = startingMinutes * 1;
+}
 
 function firstCountDown() {
     let minutes = Math.floor(time/60);
@@ -28,12 +33,32 @@ function secondCountDown() {
     modalContainer.classList.add('show')
     homeContainer.classList.add('shadow')
     
-    startingMinutes = 2;
-    time = startingMinutes * 60;
+    startingMinutes = 1;
+    time = startingMinutes * 4;
 
+    setTimeout(() => {
+        startAgainButton.classList.add('show')
+    },4000)
     firstCountDown()
+    startAgainButton.addEventListener('click', function() {
+        start()
+        
+        let minutes = Math.floor(time/60);
+        let seconds = time % 60;
+
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+
+        timeEl.textContent = `${minutes} : ${seconds}`;
+        modalTimeEl.textContent = `${minutes} : ${seconds}`;
+
+        time--;
+        modalContainer.classList.remove('show')
+        homeContainer.classList.remove('shadow')
+    })
 }
     startButton.addEventListener('click', function() {
     firstCountDown()
+    startButton.classList.add('hidden')
     setInterval(firstCountDown, 1000)
 }, {once: true})
+
