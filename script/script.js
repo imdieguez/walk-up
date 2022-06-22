fefc4e3e6012b9c322b69540249fce0df2906205
Marcelo Dieguez <mdieguezdev@gmail.com>
2022-06-22 13:52:03 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,12 +1,18 @@
 let timeEl = document.querySelector('#time-el')
-let startEl = document.querySelector('#start-el')
+let startButton = document.querySelector('#start-el')
 let modal = document.querySelector('.modal')
 let modalContainer = document.querySelector('.modal-container')
 let homeContainer = document.querySelector('.home-container')
 let modalTimeEl = document.querySelector('#modal-time-el')
+let startAgainButton = document.querySelector('.start-again-el')
 
-let startingMinutes = 30;
-let time = startingMinutes * 60;
+let startingMinutes = 10;
+let time = startingMinutes * 1;
+
+function start() {
+    startingMinutes = 20;
+    time = startingMinutes * 1;
+}
 
 function firstCountDown() {
     let minutes = Math.floor(time/60);
@@ -27,13 +33,32 @@ function secondCountDown() {
     modalContainer.classList.add('show')
     homeContainer.classList.add('shadow')
     
-    startingMinutes = 2;
-    time = startingMinutes * 60;
+    startingMinutes = 1;
+    time = startingMinutes * 4;
 
+    setTimeout(() => {
+        startAgainButton.classList.add('show')
+    },4000)
     firstCountDown()
+    startAgainButton.addEventListener('click', function() {
+        start()
+        
+        let minutes = Math.floor(time/60);
+        let seconds = time % 60;
+
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+
+        timeEl.textContent = `${minutes} : ${seconds}`;
+        modalTimeEl.textContent = `${minutes} : ${seconds}`;
+
+        time--;
+        modalContainer.classList.remove('show')
+        homeContainer.classList.remove('shadow')
+    })
 }
-
-startEl.addEventListener('click', function() {
+    startButton.addEventListener('click', function() {
     firstCountDown()
+    startButton.classList.add('hidden')
     setInterval(firstCountDown, 1000)
-})
+}, {once: true})
+
